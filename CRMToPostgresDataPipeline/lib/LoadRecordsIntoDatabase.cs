fccc10460f28858ddebf72490088f38fac0a46d4
elcorbs <emma@madetech.com>
2020-09-01 16:52:47 +0100

    attempt to speed up

--- CRMToPostgresDataPipeline/lib/LoadRecordsIntoDatabase.cs
+++ CRMToPostgresDataPipeline/lib/LoadRecordsIntoDatabase.cs
@@ -12,6 +12,11 @@ namespace CRMToPostgresDataPipeline.lib
     public class LoadRecordsIntoDatabase : ILoadRecordsIntoDatabase
     {
         private readonly ResidentContactContext _residentContactContext;
+        private int _telephoneId;
+        private int _mobileId;
+        private int _emailId;
+        private int _crmExternalSystemLookUpId;
+        private int _uhExternalSystemLookUpId;
 
         public LoadRecordsIntoDatabase(ResidentContactContext residentContactContext)
         {
@@ -22,8 +27,10 @@ namespace CRMToPostgresDataPipeline.lib
         {
             CheckContactLookUpTablesArePresent();
             CheckSourceSystemLookUpTablesArePresent();
+            GetLookupIds();
 
-            foreach (var residentContact in recordsToLoadIntoDB)
+            var residentsToAdd = new List<DbResident>();
+            var residents = recordsToLoadIntoDB.Select(residentContact =>
             {
                 var residentDetail = residentContact.Resident;
 
@@ -34,13 +41,31 @@ namespace CRMToPostgresDataPipeline.lib
                     DateOfBirth = residentDetail.DateOfBirth,
                     Gender = residentDetail.Gender
                 };
+                residentsToAdd.Add(databaseRecord);
+                return new ResidentDetails
+                {
+                    DatabaseResident = databaseRecord,
+                    DomainResident = residentContact
+                };
+            }).ToList();
 
-                _residentContactContext.Residents.Add(databaseRecord);
-                _residentContactContext.SaveChanges();
+            _residentContactContext.Residents.AddRange(residentsToAdd);
+            _residentContactContext.SaveChanges();
 
-                LoadContactDetailsAndTypesDataForResident(residentContact, databaseRecord.Id);
-                LoadExternalAndSourceSystemsDataForResident(residentContact, databaseRecord.Id);
-            }
+            var contactDetailRecords = new List<ContactDetail>();
+            residents
+                .ForEach(resident =>
+                    contactDetailRecords.AddRange(LoadContactDetailsAndTypesDataForResident(resident.DomainResident,
+                        resident.DatabaseResident.Id)));
+
+            var externalIdRecords = new List<ExternalSystemRecord>();
+            residents
+                .ForEach(resident =>
+                    externalIdRecords.AddRange(LoadExternalAndSourceSystemsDataForResident(resident.DomainResident, resident.DatabaseResident.Id)));
+
+            _residentContactContext.ContactDetails.AddRange(contactDetailRecords);
+            _residentContactContext.ExternalSystemRecords.AddRange(externalIdRecords);
+            _residentContactContext.SaveChanges();
         }
 
         private void CheckContactLookUpTablesArePresent()
@@ -77,139 +102,111 @@ namespace CRMToPostgresDataPipeline.lib
             _residentContactContext.SaveChanges();
         }
 
-        private void LoadContactDetailsAndTypesDataForResident(ResidentContact residentContact, int residentId)
+        private List<ContactDetail> LoadContactDetailsAndTypesDataForResident(ResidentContact residentContact, int residentId)
         {
-            var contactDetails = residentContact.CommunicationDetails;
+            var contactDetailsForResident = residentContact.CommunicationDetails;
+            var contactRecordsToSave = new List<ContactDetail>();
 
-            if (residentContact.CommunicationDetails.telephone != null)
-            {
-                var contactDetailsToAdd = (
-                    from number in contactDetails.telephone.Where(t => t != null)
-                    let telephoneLookupId = _residentContactContext.ContactTypeLookups
-                        .First(x => x.Name.Equals("Telephone")).Id
-                    select new ContactDetail
-                    {
-                        ContactTypeLookupId = telephoneLookupId,
-                        ContactValue = number,
-                        IsDefault = string.Equals(number, contactDetails.Default?.telephone),
-                        IsActive = string.Equals(number, contactDetails.Default?.telephone),
-                        DateAdded = DateTime.UtcNow,
-                        //Making this the same as assumption is this would be run on an empty database
-                        DateLastModified = DateTime.UtcNow,
-                        ResidentId = residentId
-                    });
-                _residentContactContext.ContactDetails.AddRange(contactDetailsToAdd);
-                _residentContactContext.SaveChanges();
-            }
-
-            if (residentContact.CommunicationDetails.mobile != null)
-            {
-                try
+            contactRecordsToSave.AddRange(residentContact.CommunicationDetails.telephone.Where(t => t != null)
+                .Select(t => new ContactDetail
                 {
-                    var mobileDetails = (
-                        from number in contactDetails.mobile.Where(m => m != null)
-                        let mobileLookupId = _residentContactContext.ContactTypeLookups
-                            .First(x => x.Name.Equals("Mobile")).Id
-                        select new ContactDetail
-                        {
-                            ContactTypeLookupId = mobileLookupId,
-                            ContactValue = number,
-                            IsDefault = number.Equals(contactDetails.Default?.mobile),
-                            IsActive = number.Equals(contactDetails.Default?.mobile),
-                            DateAdded = DateTime.UtcNow,
-                            //Making this the same as assumption is this would be run on an empty database
-                            DateLastModified = DateTime.UtcNow,
-                            ResidentId = residentId
-                        });
-                    {
-                        _residentContactContext.ContactDetails.AddRange(mobileDetails);
-                        _residentContactContext.SaveChanges();
-                    }
-                }
-                catch (Exception e)
-                {
-                    Console.WriteLine(e);
-                    Console.Write(JsonConvert.SerializeObject(residentContact));
-                    throw;
-                }
+                    ContactTypeLookupId = _telephoneId,
+                    ContactValue = t,
+                    IsDefault = string.Equals(t, contactDetailsForResident.Default?.telephone),
+                    IsActive = string.Equals(t, contactDetailsForResident.Default?.telephone),
+                    DateAdded = DateTime.UtcNow,
+                    //Making this the same as assumption is this would be run on an empty database
+                    DateLastModified = DateTime.UtcNow,
+                    ResidentId = residentId
+                }));
 
-            }
-
-            if (residentContact.CommunicationDetails.email != null)
-            {
-                var emails = (
-                    from number in contactDetails.email.Where(e => e != null)
-                    let emailLookupId = _residentContactContext.ContactTypeLookups
-                        .First(x => x.Name.Equals("Email")).Id
-                    select new ContactDetail
-                    {
-                        ContactTypeLookupId = emailLookupId,
-                        ContactValue = number,
-                        IsDefault = number.Equals(contactDetails.Default?.email),
-                        IsActive = number.Equals(contactDetails.Default?.email),
-                        DateAdded = DateTime.UtcNow,
-                        //Making this the same as assumption is this would be run on an empty database
-                        DateLastModified = DateTime.UtcNow,
-                        ResidentId = residentId
-                    });
+            contactRecordsToSave.AddRange(residentContact.CommunicationDetails.mobile.Where(m => m != null)
+                .Select(m => new ContactDetail
                 {
-                    _residentContactContext.ContactDetails.AddRange(emails);
-                    _residentContactContext.SaveChanges();
-                }
-            }
+                    ContactTypeLookupId = _mobileId,
+                    ContactValue = m,
+                    IsDefault = m.Equals(contactDetailsForResident.Default?.mobile),
+                    IsActive = m.Equals(contactDetailsForResident.Default?.mobile),
+                    DateAdded = DateTime.UtcNow,
+                    //Making this the same as assumption is this would be run on an empty database
+                    DateLastModified = DateTime.UtcNow,
+                    ResidentId = residentId
+                }));
+
+            contactRecordsToSave.AddRange(residentContact.CommunicationDetails.email.Where(e => e != null)
+                .Select(e => new ContactDetail
+                {
+                    ContactTypeLookupId = _emailId,
+                    ContactValue = e,
+                    IsDefault = e.Equals(contactDetailsForResident.Default?.email),
+                    IsActive = e.Equals(contactDetailsForResident.Default?.email),
+                    DateAdded = DateTime.UtcNow,
+                    //Making this the same as assumption is this would be run on an empty database
+                    DateLastModified = DateTime.UtcNow,
+                    ResidentId = residentId
+                }));
+
+            return contactRecordsToSave;
         }
 
-        private void LoadExternalAndSourceSystemsDataForResident(ResidentContact residentContact, int residentId)
+        private List<ExternalSystemRecord> LoadExternalAndSourceSystemsDataForResident(ResidentContact residentContact, int residentId)
         {
             var externalDetails = residentContact.DetailsFromExternalRecords;
-
-            var uhExternalSystemLookUpId =
-                _residentContactContext.ExternalSystemLookups.First(x => x.Name.Equals("UH")).Id;
-
-            var crmExternalSystemLookUpId =
-                _residentContactContext.ExternalSystemLookups.First(x => x.Name.Equals("CRM")).Id;
+            var systemRecordsToAdd = new List<ExternalSystemRecord>();
 
             if (externalDetails.HouseRef != null)
             {
-                var dbExternalSystemsRecord = new ExternalSystemRecord
+                systemRecordsToAdd.Add(new ExternalSystemRecord
                 {
                     ResidentId = residentId,
-                    ExternalSystemLookupId = uhExternalSystemLookUpId,
+                    ExternalSystemLookupId = _uhExternalSystemLookUpId,
                     Name = "HouseRef",
                     Value = externalDetails.HouseRef
-                };
-
-                _residentContactContext.ExternalSystemRecords.Add(dbExternalSystemsRecord);
-                _residentContactContext.SaveChanges();
+                });
             }
 
             if (externalDetails.PersonNo != null)
             {
-                var dbExternalSystemsRecord = new ExternalSystemRecord
+                systemRecordsToAdd.Add(new ExternalSystemRecord
                 {
                     ResidentId = residentId,
-                    ExternalSystemLookupId = uhExternalSystemLookUpId,
+                    ExternalSystemLookupId = _uhExternalSystemLookUpId,
                     Name = "PersonNo",
                     Value = externalDetails.PersonNo
-                };
-
-                _residentContactContext.ExternalSystemRecords.Add(dbExternalSystemsRecord);
-                _residentContactContext.SaveChanges();
+                });
             }
 
             if (externalDetails.ContactId != null)
             {
-                var dbExternalSystemsRecord = new ExternalSystemRecord
+                systemRecordsToAdd.Add(new ExternalSystemRecord
                 {
                     ResidentId = residentId,
-                    ExternalSystemLookupId = crmExternalSystemLookUpId,
+                    ExternalSystemLookupId = _crmExternalSystemLookUpId,
                     Name = "ContactId",
                     Value = externalDetails.ContactId
-                };
-
-                _residentContactContext.ExternalSystemRecords.Add(dbExternalSystemsRecord);
-                _residentContactContext.SaveChanges();
+                });
             }
+            return systemRecordsToAdd;
         }
+        private void GetLookupIds()
+        {
+            _telephoneId = _residentContactContext.ContactTypeLookups
+                .First(x => x.Name.Equals("Telephone")).Id;
+            _mobileId = _residentContactContext.ContactTypeLookups
+                .First(x => x.Name.Equals("Mobile")).Id;
+            _emailId = _residentContactContext.ContactTypeLookups
+                .First(x => x.Name.Equals("Email")).Id;
+            _uhExternalSystemLookUpId =
+                _residentContactContext.ExternalSystemLookups.First(x => x.Name.Equals("UH")).Id;
+            _crmExternalSystemLookUpId =
+                _residentContactContext.ExternalSystemLookups.First(x => x.Name.Equals("CRM")).Id;
+        }
+    }
+
+    public class ResidentDetails
+    {
+        public ResidentContact DomainResident { get; set; }
+
+        public DbResident DatabaseResident { get; set; }
     }
 }
